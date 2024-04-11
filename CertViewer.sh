f1831d9d871094830ba852ba0ddfd3f192615110
Dale Wyttenbach <dale.wyttenbach@va.gov>
2024-04-11 09:11:16 -0500

    Cert CLI dump (WIP)

--- CertViewer.sh
+++ CertViewer.sh
@@ -1,1 +1,14 @@
-java -cp classes com.ca.commons.security.cert.CertViewer "$@"
+#!/bin/bash
+function classpath { 
+    JAVA_PATH_SEP=${JAVA_PATH_SEP:=";"};
+    for i in "$@";
+    do
+        export CLASSPATH="${CLASSPATH}${JAVA_PATH_SEP}$(cygpath -m "$i")";
+    done
+}
+
+export CLASSPATH=
+classpath classes jackson/lib/*.jar
+echo $CLASSPATH
+
+java com.ca.commons.security.cert.CertViewer "$@"
