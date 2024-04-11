fd0d869ce4a1943c5c11b5ca68b7094b0e2c3790
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-04-11 13:14:14 -0500

    mode

--- src/com/ca/directory/BuildNumber.java
+++ src/com/ca/directory/BuildNumber.java
@@ -5,6 +5,6 @@
     
         public class BuildNumber
         {
-            public static String value = "JXplorer (build JXv3.3.1.2 2024-04-11 09:12 CDT)";
+            public static String value = "JXplorer (build JXv3.3.1.2 2024-04-11 13:13 CDT)";
         }
         
